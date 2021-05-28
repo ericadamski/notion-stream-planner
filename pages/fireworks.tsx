13e837c2e554a5cb5636cb1831b88674a6136a97
Eric Adamski <er.adamski@gmail.com>
2021-05-28 14:53:57 -0400

    Fix the deploy

--- pages/fireworks.tsx
+++ pages/fireworks.tsx
@@ -1,14 +1,26 @@
 import { useState, useEffect, useRef } from "react";
 import ReactCanvasConfetti from "react-canvas-confetti";
 import { CreateTypes } from "canvas-confetti";
+import { motion } from "framer-motion";
+// @ts-ignore
+import { EmojiProvider, Emoji } from "react-apple-emojis";
+import emojiData from "react-apple-emojis/lib/data.json";
 
 import { subscribe } from "lib/ably";
+
+interface Reaction {
+  emoji: string;
+  id: string;
+}
+
+const MAX_EMOJI_RENDER = Array.from({ length: 60 });
 
 export default function FireworksPage() {
   const confettiCanvas = useRef<CreateTypes | null>();
   const fireworkTimer = useRef<number | NodeJS.Timer>();
   const fireworkIntervalTimer = useRef<number | NodeJS.Timer>();
   const [isFiring, setIsFiring] = useState<boolean>(false);
+  const [emojis, setEmojis] = useState<Reaction[]>([]);
 
   useEffect(() => {
     subscribe("fireworks", () => {
@@ -21,8 +33,24 @@ export default function FireworksPage() {
       setIsFiring(true);
     });
 
-    subscribe("emojis", (data) => {
-      console.log(data);
+    subscribe("emoji", ({ id, data }) => {
+      try {
+        const { emoji } = JSON.parse(data);
+
+        setEmojis((current) => {
+          const copy = current.slice(-60);
+          copy.push({
+            emoji,
+            id,
+          });
+
+          console.log({ copy });
+
+          return copy;
+        });
+      } catch {
+        // lost the emoij
+      }
     }); // throttle, RxJS
   }, []);
 
@@ -55,7 +83,7 @@ export default function FireworksPage() {
           });
         }, 400);
       } else {
-        setTimeout(() => confetti.reset(), 3000);
+        setTimeout(() => confetti?.reset(), 3000);
         if (fireworkIntervalTimer.current != null) {
           clearInterval(fireworkIntervalTimer.current as number);
         }
@@ -64,18 +92,39 @@ export default function FireworksPage() {
   }, [isFiring]);
 
   return (
-    <ReactCanvasConfetti
-      refConfetti={(instance) => (confettiCanvas.current = instance)}
-      style={{
-        position: "fixed",
-        pointerEvents: "none",
-        width: "100%",
-        height: "100%",
-        top: 0,
-        left: 0,
-      }}
-    />
-    // framer-motion
+    <>
+      <ReactCanvasConfetti
+        refConfetti={(instance) => (confettiCanvas.current = instance)}
+        style={{
+          position: "fixed",
+          pointerEvents: "none",
+          width: "100%",
+          height: "100%",
+          top: 0,
+          left: 0,
+        }}
+      />
+      <div style={{ height: "100vh" }}>
+        <EmojiProvider data={emojiData}>
+          {MAX_EMOJI_RENDER.map((_, idx) => {
+            const reaction = emojis[idx] ?? {};
+
+            return (
+              <motion.div
+                key={idx}
+                animate={{ x: 100, y: "0%" }}
+                style={{ y: "100%" }}
+                transition={{ duration: 3 }}
+              >
+                {reaction.emoji && (
+                  <Emoji key={reaction.id} name={reaction.emoji} />
+                )}
+              </motion.div>
+            );
+          })}
+        </EmojiProvider>
+      </div>
+    </>
   );
 }
 
