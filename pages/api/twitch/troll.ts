f2e39d92ab4b62ecdaffd0e04f77addfc7952673
Eric Adamski <er.adamski@gmail.com>
2021-06-07 22:43:36 -0400

    Add table flip"

---
+++ pages/api/twitch/troll.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { publish } from "lib/ably";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "GET") {
+    return res.status(405).end();
+  }
+
+  publish("messup", { text: req.query.t }, "messup-eric");
+
+  res.end("TableHere FlipThis FBtouchdown");
+};
