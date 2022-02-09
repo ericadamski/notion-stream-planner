0da9e43e994bca0a72a0a3896876074292a0bc4a
Eric Adamski <er.adamski@gmail.com>
2022-02-08 21:20:56 -0500

    hack auto hotdog theme

---
+++ pages/api/twitch/hotdog.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { publish } from "lib/ably";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== "GET") {
+    return res.status(405).end();
+  }
+
+  publish("hotdog", { tick: Date.now() }, "messup-eric");
+
+  res.end();
+};
